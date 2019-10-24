3daef3aebb785b0a8056d3569f63c7b32c17051c
root <axel949@hotmail.com>
2019-10-24 12:42:51 +0000

    updated the script

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 ./ansible_install.sh
-echo "130.238.28.88 15ansible" >> /etc/hosts
-echo "130.238.28.95 15sparkmaster" >> /etc/hosts
-echo "130.238.28.158 15sparkworker" >> /etc/hosts
+echo "192.168.1.72 15ansible" >> /etc/hosts
+echo "192.168.1.74 15sparkmaster" >> /etc/hosts
+echo "192.168.1.62 15sparkworker" >> /etc/hosts
+echo "15sparkmaster ansible_ssh_host=192.168.1.74" >> /etc/ansible/hosts
+echo "15sparkworker ansible_ssh_host=192.168.1.62" >> /etc/ansible/hosts
+echo "[sparkmaster]" >> /etc/ansible/hosts
+echo "15sparkmaster ansible_connection=local ansible_user=ubuntu" >> /etc/ansible/hosts
+echo "[sparkworker]" >> /etc/ansible/hosts
+echo "15sparkworker ansible_connection=local ansible_user=ubuntu" >> /etc/ansible/hosts
 echo "[sparkmaster]" >> /etc/ansible/hosts
 echo "15sparkmaster" >> /etc/ansible/hosts
 echo "[sparkworker]" >> /etc/ansible/hosts
