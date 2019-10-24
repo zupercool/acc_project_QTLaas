ad75504f19103717b7a7bb0e8a172c4efa663ef7
root <axel949@hotmail.com>
2019-10-24 12:29:00 +0000

    Changed some files and added example host files

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./ansible_install.sh
-echo "130.238.28.115 group-15-ansible" >> /etc/hosts
-echo "130.238.28.103 15sparkmaster" >> /etc/hosts
-echo "130.238.28.135 15sparkworker" >> /etc/hosts
+echo "130.238.28.88 15ansible" >> /etc/hosts
+echo "130.238.28.95 15sparkmaster" >> /etc/hosts
+echo "130.238.28.158 15sparkworker" >> /etc/hosts
 echo "[sparkmaster]" >> /etc/ansible/hosts
 echo "15sparkmaster" >> /etc/ansible/hosts
 echo "[sparkworker]" >> /etc/ansible/hosts
